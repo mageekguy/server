36d78b75e67b5638cb5ce9be352f0994c7af421d
mageekguy <frederic.hardy@mageekbox.net>
2014-01-09 16:57:51 +0100

    Update autoloader path.

--- demo/bootstrap.php
+++ demo/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
-call_user_func_array(include(__DIR__ . '/../autoloader.php'), [ [ 'server\demo' =>  __DIR__ . '/classes' ] ]);
+call_user_func_array(include(__DIR__ . '/../autoloader/autoloader.php'), [ [ 'server\demo' =>  __DIR__ . '/classes' ] ]);
 
 require_once __DIR__ . '/../bootstrap.php';
